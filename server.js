ffd851316abef62047630b8c609a0032ccc09073
Marcus Ngooi <mngooi@gmail.com>
2021-11-16 14:39:14 -0500

    Updated internal documentation for .js and .css files

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
  */
 
 let app = require("./server/config/app");
-let debug = require("debug")("assignment-2:server");
+let debug = require("debug")("team-project-part-2-first-release:server");
 let http = require("http");
 
 /**
--- server/config/app.js
+++ server/config/app.js
@@ -1,9 +1,9 @@
 /*  COMP229-013 F2021
-    Group Project Part 2 First Release
-    File Name:  config/app.js
-    Student#:   
-    Name:       
-    Date:       
+    Group Project Part 2 First Release - Smart Survey
+    File Name:   app.css
+    Student#:    301147411, 301182173, 301163120, 301168420, 301182196, 301159644 
+    Name:        Marcus Ngooi, Tatsiana Ptushko, Josef Signo, Sukhmannat Singh, Yuko Yamano, Agustin Ignacio Zuluaga
+    Description: Contains code for initial set up of web application including required modules, database setup, engine setup, routing, error handling      
  */
 
 // require modules
